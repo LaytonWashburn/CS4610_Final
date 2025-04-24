a76dc04f5c1a300d8870669c58905a126a7cd222
Layton Washburn <layton.washburn1659@gmail.com>
2025-04-23 20:34:23 -0600

    Saving work for the night

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     return (
         <nav className="bg-secondary-blue2 h-[4rem] flex justify-between items-center px-4">
           <div className="flex space-x-4 h-[4rem] items-center">
-            <h1 className="font-bold text-2xl text-center">Aggie Tutor</h1>
+            <Link className="font-bold text-2xl text-center hover:text-secondary-grey" to={"/dashboard/"}>Aggie Tutor</Link>
               <Link className="text-secondary-pink font-bold text-center flex items-center hover:text-white" to={"/dashboard/"}>Home</Link>
               <Link className="text-secondary-pink font-bold text-center flex items-center hover:text-white" to={"/dashboard/tutors/"}>Tutors</Link>
               <Link className="text-secondary-pink font-bold text-center flex items-center hover:text-white" to={"/dashboard/chat/"}>Chat</Link>
--- client/src/pages/Tutors/TutorCard.tsx
+++ client/src/pages/Tutors/TutorCard.tsx
@@ -11,7 +11,7 @@ export const TutorCard = ({ id, firstName, lastName, email }) : ItutorCardProps 
 
 
     return(
-        <main className="flex flex-col w-[50vw] items-center justify-center shadow bg-secondary-pink text-white rounded-lg">
+        <main className="flex flex-col w-[50vw] items-center justify-center shadow bg-secondary-pink text-white rounded-2xl m-2 shadow-lg">
             <p>Meet: {firstName } { lastName } </p>
             <p>Email: { email }</p>
         </main>
